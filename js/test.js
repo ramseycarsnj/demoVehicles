c293589d9679e220e788968487145d9b6cd03f9a
Joe <joethemovie@gmail.com>
2016-10-17 16:38:54 -0400

    added and removed appends for photo content. still unfinished.

--- js/test.js
+++ js/test.js
@@ -96,46 +96,81 @@ $(document).ready(function(){
 function listing(thisCar){
   // insert append function code here
 
-$('.supportingImgs').append(
+
+$('.mainImg').append(
   '<img src="'
-  + thisCar.supportImg1 + '" alt="' + thisCar.car + ' Image 1" class="' + thisCar.car + 'Sup"/><img src="'
-  + thisCar.supportImg2 + '" alt="' + thisCar.car + ' Image 2" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg3 + '" alt="' + thisCar.car + ' Image 3" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg4 + '" alt="' + thisCar.car + ' Image 4" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg5 + '" alt="' + thisCar.car + ' Image 5" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg6 + '" alt="' + thisCar.car + ' Image 6" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg7 + '" alt="' + thisCar.car + ' Image 7" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg8 + '" alt="' + thisCar.car + ' Image 8" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg9 + '" alt="' + thisCar.car + ' Image 9" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg10 + '" alt="' + thisCar.car + ' Image 10" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg11 + '" alt="' + thisCar.car + ' Image 11" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg12 + '" alt="' + thisCar.car + ' Image 12" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg13 + '" alt="' + thisCar.car + ' Image 13" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg14 + '" alt="' + thisCar.car + ' Image 14" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg15 + '" alt="' + thisCar.car + ' Image 15" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg16 + '" alt="' + thisCar.car + ' Image 16" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg17 + '" alt="' + thisCar.car + ' Image 17" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg18 + '" alt="' + thisCar.car + ' Image 18" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg19 + '" alt="' + thisCar.car + ' Image 19" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg20 + '" alt="' + thisCar.car + ' Image 20" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg21 + '" alt="' + thisCar.car + ' Image 21" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg22 + '" alt="' + thisCar.car + ' Image 22" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg23 + '" alt="' + thisCar.car + ' Image 23" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg24 + '" alt="' + thisCar.car + ' Image 24" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg25 + '" alt="' + thisCar.car + ' Image 25" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg26 + '" alt="' + thisCar.car + ' Image 26" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg27 + '" alt="' + thisCar.car + ' Image 27" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg28 + '" alt="' + thisCar.car + ' Image 28" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg29 + '" alt="' + thisCar.car + ' Image 29" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg30 + '" alt="' + thisCar.car + ' Image 30" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg31 + '" alt="' + thisCar.car + ' Image 31" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg32 + '" alt="' + thisCar.car + ' Image 32" class="' + thisCar.car + 'Sup" /><img src="'
-  + thisCar.supportImg33 + '" alt="' + thisCar.car + ' Image 33" class="' + thisCar.car + 'Sup" />');
+  + thisCar.supportImg1 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg1 mains" /><img src="'
+  + thisCar.supportImg2 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg2 mains" /><img src="'
+  + thisCar.supportImg3 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg3 mains" /><img src="'
+  + thisCar.supportImg4 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg4 mains" /><img src="'
+  + thisCar.supportImg5 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg5 mains" /><img src="'
+  + thisCar.supportImg6 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg6 mains" /><img src="'
+  + thisCar.supportImg7 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg7 mains" /><img src="'
+  + thisCar.supportImg8 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg8 mains" /><img src="'
+  + thisCar.supportImg9 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg9 mains" /><img src="'
+  + thisCar.supportImg10 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg10 mains" /><img src="'
+  + thisCar.supportImg11 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg11 mains" /><img src="'
+  + thisCar.supportImg12 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg12 mains" /><img src="'
+  + thisCar.supportImg13 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg13 mains" /><img src="'
+  + thisCar.supportImg14 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg14 mains" /><img src="'
+  + thisCar.supportImg15 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg15 mains" /><img src="'
+  + thisCar.supportImg16 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg16 mains" /><img src="'
+  + thisCar.supportImg17 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg17 mains" /><img src="'
+  + thisCar.supportImg18 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg18 mains" /><img src="'
+  + thisCar.supportImg19 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg19 mains" /><img src="'
+  + thisCar.supportImg20 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg20 mains" /><img src="'
+  + thisCar.supportImg21 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg21 mains" /><img src="'
+  + thisCar.supportImg22 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg22 mains" /><img src="'
+  + thisCar.supportImg23 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg23 mains" /><img src="'
+  + thisCar.supportImg24 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg24 mains" /><img src="'
+  + thisCar.supportImg25 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg25 mains" /><img src="'
+  + thisCar.supportImg26 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg26 mains" /><img src="'
+  + thisCar.supportImg27 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg27 mains" /><img src="'
+  + thisCar.supportImg28 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg28 mains" /><img src="'
+  + thisCar.supportImg29 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg29 mains" /><img src="'
+  + thisCar.supportImg30 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg30 mains" /><img src="'
+  + thisCar.supportImg31 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg31 mains" /><img src="'
+  + thisCar.supportImg32 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg32 mains" /><img src="'
+  + thisCar.supportImg33 + '" alt="' + thisCar.car + ' Image" class="' + thisCar.stock + 'MainImg33 mains" /><img src="'
+  + thisCar.supportImg1 + '" alt="' + thisCar.car + ' Image 1" class="' + thisCar.stock + 'Sup1 supSize"/><img src="'
+  + thisCar.supportImg2 + '" alt="' + thisCar.car + ' Image 2" class="' + thisCar.stock + 'Sup2 supSize" /><img src="'
+  + thisCar.supportImg3 + '" alt="' + thisCar.car + ' Image 3" class="' + thisCar.stock + 'Sup3 supSize" /><img src="'
+  + thisCar.supportImg4 + '" alt="' + thisCar.car + ' Image 4" class="' + thisCar.stock + 'Sup4 supSize" /><img src="'
+  + thisCar.supportImg5 + '" alt="' + thisCar.car + ' Image 5" class="' + thisCar.stock + 'Sup5 supSize" /><img src="'
+  + thisCar.supportImg6 + '" alt="' + thisCar.car + ' Image 6" class="' + thisCar.stock + 'Sup6 supSize" /><img src="'
+  + thisCar.supportImg7 + '" alt="' + thisCar.car + ' Image 7" class="' + thisCar.stock + 'Sup7 supSize" /><img src="'
+  + thisCar.supportImg8 + '" alt="' + thisCar.car + ' Image 8" class="' + thisCar.stock + 'Sup8 supSize" /><img src="'
+  + thisCar.supportImg9 + '" alt="' + thisCar.car + ' Image 9" class="' + thisCar.stock + 'Sup9 supSize" /><img src="'
+  + thisCar.supportImg10 + '" alt="' + thisCar.car + ' Image 10" class="' + thisCar.stock + 'Sup10 supSize" /><img src="'
+  + thisCar.supportImg11 + '" alt="' + thisCar.car + ' Image 11" class="' + thisCar.stock + 'Sup11 supSize" /><img src="'
+  + thisCar.supportImg12 + '" alt="' + thisCar.car + ' Image 12" class="' + thisCar.stock + 'Sup12 supSize" /><img src="'
+  + thisCar.supportImg13 + '" alt="' + thisCar.car + ' Image 13" class="' + thisCar.stock + 'Sup13 supSize" /><img src="'
+  + thisCar.supportImg14 + '" alt="' + thisCar.car + ' Image 14" class="' + thisCar.stock + 'Sup14 supSize" /><img src="'
+  + thisCar.supportImg15 + '" alt="' + thisCar.car + ' Image 15" class="' + thisCar.stock + 'Sup15 supSize" /><img src="'
+  + thisCar.supportImg16 + '" alt="' + thisCar.car + ' Image 16" class="' + thisCar.stock + 'Sup16 supSize" /><img src="'
+  + thisCar.supportImg17 + '" alt="' + thisCar.car + ' Image 17" class="' + thisCar.stock + 'Sup17 supSize" /><img src="'
+  + thisCar.supportImg18 + '" alt="' + thisCar.car + ' Image 18" class="' + thisCar.stock + 'Sup18 supSize" /><img src="'
+  + thisCar.supportImg19 + '" alt="' + thisCar.car + ' Image 19" class="' + thisCar.stock + 'Sup19 supSize" /><img src="'
+  + thisCar.supportImg20 + '" alt="' + thisCar.car + ' Image 20" class="' + thisCar.stock + 'Sup20 supSize" /><img src="'
+  + thisCar.supportImg21 + '" alt="' + thisCar.car + ' Image 21" class="' + thisCar.stock + 'Sup21 supSize" /><img src="'
+  + thisCar.supportImg22 + '" alt="' + thisCar.car + ' Image 22" class="' + thisCar.stock + 'Sup22 supSize" /><img src="'
+  + thisCar.supportImg23 + '" alt="' + thisCar.car + ' Image 23" class="' + thisCar.stock + 'Sup23 supSize" /><img src="'
+  + thisCar.supportImg24 + '" alt="' + thisCar.car + ' Image 24" class="' + thisCar.stock + 'Sup24 supSize" /><img src="'
+  + thisCar.supportImg25 + '" alt="' + thisCar.car + ' Image 25" class="' + thisCar.stock + 'Sup25 supSize" /><img src="'
+  + thisCar.supportImg26 + '" alt="' + thisCar.car + ' Image 26" class="' + thisCar.stock + 'Sup26 supSize" /><img src="'
+  + thisCar.supportImg27 + '" alt="' + thisCar.car + ' Image 27" class="' + thisCar.stock + 'Sup27 supSize" /><img src="'
+  + thisCar.supportImg28 + '" alt="' + thisCar.car + ' Image 28" class="' + thisCar.stock + 'Sup28 supSize" /><img src="'
+  + thisCar.supportImg29 + '" alt="' + thisCar.car + ' Image 29" class="' + thisCar.stock + 'Sup29 supSize" /><img src="'
+  + thisCar.supportImg30 + '" alt="' + thisCar.car + ' Image 30" class="' + thisCar.stock + 'Sup30 supSize" /><img src="'
+  + thisCar.supportImg31 + '" alt="' + thisCar.car + ' Image 31" class="' + thisCar.stock + 'Sup31 supSize" /><img src="'
+  + thisCar.supportImg32 + '" alt="' + thisCar.car + ' Image 32" class="' + thisCar.stock + 'Sup32 supSize" /><img src="'
+  + thisCar.supportImg33 + '" alt="' + thisCar.car + ' Image 33" class="' + thisCar.stock + 'Sup33 supSize" />');
 
 
-  $('.mainImg').append(
-    '<img src="' + thisCar.supportImg2 + '" alt="' + thisCar.car + ' Image" />'
-  );
+
+
+  supArray.push(thisCar.stock + 'Sup');
+  mainArray.push(thisCar.stock + 'MainImg');
 
 }
 
@@ -461,10 +496,19 @@ vehicleArray[5] = new Vehicle(
 //   'Automatic CVT',
 //   '61,268'
 // );
-
+var supArray = []
 for (var i = 0; i < vehicleArray.length; i++) {
   listing(vehicleArray[i]);
 }
+
+
+$('.mains').hide();
+
+for (var j = 0; j < supArray.length; j++) {
+  $('.' + supArray[j]).hide();
+  $('.' + supArray[0]).show();
+}
+
 
 
 });
